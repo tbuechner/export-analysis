5e53f0060ee9571e337049a08d7767e93056fc7c
Thomas Buechner <thomas.buechner@collaboration-factory.de>
2024-03-12 08:49:34 +0100

    new version

--- cplace-code/concatenate.py
+++ cplace-code/concatenate.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 
 from tokenizer import count_tokens_in_file
@@ -13,7 +14,10 @@ def concatenate(folder_name):
 
     concatenated = concatenate_files_in_folder(folder_name)
 
-    # write the concatenated contents to a file
+    concatenated = replace_multiline_whitespace(concatenated)
+
+
+# write the concatenated contents to a file
     with open(output_file_name, 'w') as outfile:
         outfile.write(concatenated)
 
@@ -55,25 +59,7 @@ def concatenate_file(file, folder_name):
         # read the contents of the file
         contents = infile.read()
 
-        # if file ends with .java filter all lines which start with "import "
-        if file.endswith('.java'):
-            lines = contents.split('\n')
-            contents = ''
-            for line in lines:
-                if not line.startswith('import '):
-                    contents += line + '\n'
-
-        if file.endswith('.ts'):
-            lines = contents.split('\n')
-            contents = ''
-            for line in lines:
-                if line.strip().startswith('import '):
-                    skip = True
-                if skip and line.strip().endswith(';'):
-                    skip = False
-                    continue
-                if not skip:
-                    contents += line + '\n'
+        contents = filter_code(contents, file)
 
         result += contents
 
@@ -81,6 +67,38 @@ def concatenate_file(file, folder_name):
         result += '######### end - content of file "' + folder_name + '/' + file + '" #########\n'
         result += '\n'
     return result
+
+
+def remove_copyright_comment(content):
+    copyright_comment_pattern = r"/\*\n \* Copyright \d{4}, collaboration Factory AG. All rights reserved.\n \*/"
+    return re.sub(copyright_comment_pattern, '', content)
+
+def replace_multiline_whitespace(content):
+    return re.sub(r'(\n[\s]*\n)+', '\n\n', content)
+
+def filter_code(contents, file):
+
+    # if file ends with .java filter all lines which start with "import "
+    if file.endswith('.java'):
+        contents = remove_copyright_comment(contents)
+
+        lines = contents.split('\n')
+        contents = ''
+        for line in lines:
+            if not line.startswith('import '):
+                contents += line + '\n'
+    if file.endswith('.ts'):
+        lines = contents.split('\n')
+        contents = ''
+        for line in lines:
+            if line.strip().startswith('import '):
+                skip = True
+            if skip and line.strip().endswith(';'):
+                skip = False
+                continue
+            if not skip:
+                contents += line + '\n'
+    return contents
 
 
 concatenate('group-all')
