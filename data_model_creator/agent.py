c400afe87c0d878fc33db4265774014d74c9a961
Thomas Buechner <thomas.buechner@collaboration-factory.de>
2024-04-23 16:35:41 +0200

    new version

--- data_model_creator/agent.py
+++ data_model_creator/agent.py
@@ -1,3 +1,5 @@
+import json
+
 from langchain_core.prompts import (
     ChatPromptTemplate,
 )
@@ -5,10 +7,38 @@ from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate
 from langchain_core.messages import SystemMessage
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_core.language_models import LanguageModelLike
+from langchain_openai import AzureChatOpenAI
+from pydantic.json import pydantic_encoder
 
 from data_model_creator.tools.models import TypeDefinitions
 from tools.attribute import get_attribute_tools
 from tools.type import get_type_tools
+
+
+def invoke(input: str):
+    type_definitions: TypeDefinitions = {}
+
+    llm = AzureChatOpenAI(
+        temperature=0.0, model="gpt-35-turbo", api_version="2024-02-01"
+    )
+    # llm.client = Wrapper(llm.client)
+
+    agent_executor = create_data_model_agent(llm, type_definitions, verbose=True)
+
+    history = []
+
+    prompt = f"""{input}.
+    The current JSON representation of the data model is: {json.dumps(type_definitions, default=pydantic_encoder)}"""
+
+    result = agent_executor.invoke(
+        {
+            "input": prompt,
+            "history": history,
+        },
+    )
+
+    print("type_definitions: " + json.dumps(type_definitions, default=pydantic_encoder))
+    print("output: " + result["output"])
 
 
 def create_data_model_agent(
--- data_model_creator/dmcMain.py
+++ data_model_creator/dmcMain.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 
-from data_model_creator.api import invoke
+from data_model_creator.agent import invoke
 
 load_dotenv()
+
 invoke('Generate a data model for a risk management application. The risk type should have attributes for the probability and impact of the risk.')
